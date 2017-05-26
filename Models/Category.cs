7f402db1d599fbca314b9aee18eca603627cc48d
fgcabral88 <fgcabral@yahoo.com>
2017-05-26 20:34:36 -0300

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Felipe.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public String Nome { get; set; } 
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
